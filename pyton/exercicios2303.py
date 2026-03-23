c14b0366321c25cd15bed34c98d7e07bf01542ff
Gabriel Dario <gabrieldario66@gmail.com>
2026-03-23 10:39:49 -0300

    Métodos e Funcões
    aula 23 de março de 2026

---
+++ pyton/exercicios2303.py
@@ -0,0 +1,25 @@
+
+frase = "a tecnologia move o mundo"
+
+
+fraseUm = frase.replace("a", "A", 1)
+
+fraseUm = frase.capitalize() # Primeira palavra Maiúscula
+fraseDois = frase.title() # Para Botar toda começo de palavra maiúscula
+print(fraseUm)
+print(fraseDois)
+
+'''
+
+nome = 'Gabriel Dario da Rosa'
+primeiroNome = nome[:7]
+ultimoNome = nome[17:]
+nomeSobrenome = nome[:7] + nome[7:13]
+aoContrario = nome[::-1]
+pulando = nome[::2]
+print(f'Primeior nome: {primeiroNome}')
+print(f'Último nome: {ultimoNome}')
+print(f'Nome Sobrenome: {nomeSobrenome}')
+print(f'Inversso: {aoContrario}')
+print(f'Pulando de um em um: {pulando}')
+'''
